4748ee842e43dd596a435b4bf715d59b3f47bf21
Priyanshu Rana <priyanshurana1234@gmail.com>
2024-01-30 00:12:43 +0530

    implemented-login-api-&-password-verification

--- blog/routers/auth.py
+++ blog/routers/auth.py
@@ -2,10 +2,20 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from .. import schemas, models
 from ..database import get_db
+from ..hashing import Hash
 
 router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
 def login(request: schemas.Login, db: Session = Depends(get_db)):
-    print("login in successful")
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Creadentials"
+        )
+    if not Hash.verify(request.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect Password"
+        )
+    return user
