78b893ea335672f379fff49d39425ea0c055746e
Priyanshu Rana <priyanshurana1234@gmail.com>
2024-02-02 03:57:15 +0530

    OAuth2-setup-with-blog-&-user-api(s)

--- blog/routers/auth.py
+++ blog/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import schemas, models, token
 from ..database import get_db
@@ -8,7 +9,10 @@ router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(request: schemas.Login, db: Session = Depends(get_db)) -> schemas.Token:
+def login(
+    request: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+) -> schemas.Token:
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -11,17 +11,30 @@ router = APIRouter(prefix="/blog", tags=["Blogs"])
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create(
+    request: schemas.Blog,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
     return BlogService.create(request, db)
 
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(
+    id,
+    request: schemas.Blog,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
     return BlogService.update(id, request, db)
 
 
 @router.delete("/{id}", status_code=status.HTTP_200_OK)  # can be use 204
-def delete(id, db: Session = Depends(get_db)):
+def delete(
+    id,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
     return BlogService.delete(id, db)
 
 
@@ -42,5 +55,9 @@ def get_blogs(
     status_code=status.HTTP_200_OK,
     response_model=schemas.ShowBlogResponseModel,
 )
-def get_blog(id, db: Session = Depends(get_db)):
+def get_blog(
+    id,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
     return BlogService.get_blog(id, db)
